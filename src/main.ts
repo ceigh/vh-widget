c9dcf40ffa66187ccf1e43d5ce64171c5344664f
Artjom Löbsack <me@ceigh.com>
2021-06-28 18:38:30 +0500

    feat: bootstrap inline app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const el = document.createElement('div')
+document.body.appendChild(el)
+createApp(App).mount(el)
